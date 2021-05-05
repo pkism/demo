906b6571f4ff4d6c7a209c9977703635bc24f5a8
unknown <pappukumar052@gmail.com>
2021-05-05 22:48:03 +0530

    pushed code

---
+++ demo/src/main/java/com/example/demo/CalculatorController.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+
+	
+	  @GetMapping("/getadd/{a}/{b}")
+	  public int add(@PathVariable int a,@PathVariable int b)
+	  {
+		  return a+b;
+	  }
+	  
+	  @GetMapping("/getsub/{a}/{b}")
+	  public int sub(@PathVariable int a,@PathVariable int b)
+	  {
+		  return a-b;
+	  }
+	  
+	  @GetMapping("/getmul/{a}/{b}")
+	  public int mul(@PathVariable int a,@PathVariable int b)
+	  {
+		  return a*b;
+	  }
+	
+	  @GetMapping("/getdiv/{a}/{b}")
+	  public double div(@PathVariable double a,@PathVariable double b)
+	  {
+		  if(b!=0)
+		  {
+			  
+			  return a/b;
+		  }
+		  else
+		  {
+			  System.out.println("Invalid number");
+			  return -1;
+		  }
+	  }
+	  
+	  @GetMapping("/getsquare/{a}")
+	  public int square(@PathVariable int a)
+	  {
+		  return a*a;
+	  }
+	  
+	  @GetMapping("/getcube/{a}")
+	  public int cube(@PathVariable int a)
+	  {
+		  int temp=a*a;
+		  return temp*a;
+	  }
+}
