2cb6ce2d4ff8dcc2f13f8e31122db983bf95f264
unknown <pappukumar052@gmail.com>
2021-05-06 17:33:43 +0530

    added code

--- src/main/java/com/example/demo/CalculatorController.java
+++ src/main/java/com/example/demo/CalculatorController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/calculator")
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class CalculatorController {
 
 	
